9bf9eb5b79ba7984505c3790e8eb1258a4311509
Benedikt Spranger <bene@eurovibes.org>
2020-05-05 22:30:28 +0200

    Add basic greylistd daemon integration tests.
    Enhance the integration tests by basic deamon checks to ensure valid
    operations.
    
    Signed-off-by: Benedikt Spranger <bene@eurovibes.org>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,10 +1,21 @@
 from filecmp import cmp
-from shutil import copytree, rmtree
-from subprocess import run
+from getpass import getuser
+from pathlib import Path
+from shutil import copyfile, copytree, rmtree
+from socket import socket, AF_UNIX, SOCK_STREAM
+from subprocess import Popen, run
+from time import sleep
 
+import threading
 import unittest
 
+
+(TRUE, FALSE, BLACK, GREY, WHITE, UNSEEN) = (
+    b"true", b"false", b"black", b"grey", b"white", b"unseen")
+
+localconfig = "tmp1/config"
 setuphelper = "program/greylistd-setup-exim4"
+
 add_split_data = [setuphelper, "add", "-no-reload",
                   "tmp/40_exim4-config_check_data", "acl_check_data"]
 del_split_data = [setuphelper, "remove", "-no-reload",
@@ -22,6 +33,9 @@ add_combined_rcpt = [setuphelper, "add", "-no-reload",
                      "tmp/exim4.conf.template", "acl_check_rcpt"]
 del_combined_rcpt = [setuphelper, "remove", "-no-reload",
                      "tmp/exim4.conf.template", "acl_check_rcpt"]
+
+greylistcmd = b" --grey 192.0.2.42 send@example.com recv@example.com"
+
 
 class test_setup_exim4(unittest.TestCase):
     @classmethod
@@ -82,6 +96,84 @@ class test_setup_exim4(unittest.TestCase):
         self.assertTrue(cmp("tmp/exim4.conf.template",
                             "tests/data/exim4.conf.template",
                             shallow=False))
+
+
+def get_value(localconfig, section, key):
+    with open(localconfig) as fp:
+        for line in fp:
+            line = line.strip()
+
+            if line.startswith("#"):
+                continue
+
+            if (line[0:1] == '[') and (']' in line):
+                _section = line[1:line.find(']')].strip().lower()
+                continue
+
+            if ('=' in line):
+                _key, _data = [s.strip() for s in line.split('=', 1)]
+                if (section == _section) and (_key == key):
+                    return _data
+
+    return None
+
+
+pid = None
+
+
+class test_basic_checks(unittest.TestCase):
+    daemon = None
+    sock = None
+
+    def greylistd():
+        global pid
+        pid = Popen(["program/greylistd", localconfig])
+
+    @classmethod
+    def setUpClass(cls):
+        rmtree("tmp1", ignore_errors=True)
+        Path("tmp1").mkdir(parents=True, exist_ok=True)
+        copyfile("tests/config", "tmp1/config")
+        with open("tmp1/config", "a") as fp:
+            fp.write("owner = %s" % getuser())
+
+        # start the engines
+        cls.daemon = threading.Thread(target=cls.greylistd,
+                                      name="greylistd",
+                                      daemon=True)
+        cls.daemon.start()
+        cls.sockname = get_value(localconfig, "socket", "path")
+        sleep(5)
+
+    @classmethod
+    def tearDownClass(cls):
+        global pid
+        pid.terminate()
+        pid.kill()
+
+    def tearDown(self):
+        self.sock.close()
+
+    def setUp(self):
+        self.sock = socket(AF_UNIX, SOCK_STREAM)
+        self.sock.connect(self.sockname)
+
+    def test_first(self):
+        self.sock.send(greylistcmd)
+        status = self.sock.recv(1024)
+        self.assertEqual(status, TRUE, msg="First check.")
+
+    def test_too_fast(self):
+        self.sock.send(greylistcmd)
+        status = self.sock.recv(1024)
+        self.assertEqual(status, TRUE, msg="Retry too fast.")
+
+    def test_white(self):
+        sleep(45)
+        self.sock.send(greylistcmd)
+        status = self.sock.recv(1024)
+        self.assertEqual(status, FALSE, msg="Auto whitelist.")
+
 
 if __name__ == '__main__':
     unittest.main()
